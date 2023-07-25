a4db9e4e032dbbe64c80d5c057244794bd9ea0d2
bytesolo <8169377@qq.com>
2023-07-25 23:14:03 +0800

    继续完善组件toggle

--- components/qblocks/toggle-group/index.js
+++ components/qblocks/toggle-group/index.js
@@ -1,24 +1,72 @@
 // components/qblocks/togglebutton-group/index.js
 Component({
-
   /**
    * 组件的属性列表
    */
   properties: {
-
+    selectedItemId: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    toggle: 1,
+    children: []
   },
-
+  relations: {
+    '../toggle/index': {
+      type: 'child', // 子组件的关联类型，即是子组件
+      linked(target) {
+        // 子组件关联时触发
+        console.log('Linked to child component', target);
+      },
+      unlinked(target) {
+        // 子组件取消关联时触发
+        console.log('Unlinked from child component', target);
+      }
+    }
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onChange(event) {
+      console.log('onChange');
+      const name = event.detail.name;
+      console.log(event);
+      this.data.radios.forEach((item) => {
+        item.checked = item.name === name;
+      });
+      this.setData({
+        radios: this.data.radios
+      });
+      this.triggerEvent('change', {
+        value: name
+      });
+    },
+    updateRadios() {
+      const children = this.getRelationNodes('../toggle/index');
+      console.log(children);
+      const value = this.data.value;
+      const radios = [];
+      children.forEach((child) => {
+        const checked = child.data.name === value;
+        radios.push({
+          name: child.data.name,
+          label: child.data.label,
+          checked
+        });
+      });
+      this.setData({
+        children
+      });
+      console.log(this.data);
+    }
+  },
+  lifetimes: {
+    ready() {
+      this.updateRadios();
+    }
   }
 })
--- components/qblocks/toggle/index.js
+++ components/qblocks/toggle/index.js
@@ -8,20 +8,46 @@ Component({
    */
   properties: {
     icon: String,
-    text: String
+    label: String,
+    selected: {
+      type: Boolean,
+      value: false
+    }
   },
-
+  relations: {
+    '../toggle-group/index': {
+      type: 'parent', // 关联的目标节点应为父节点
+      linked: function (target) {
+        // 每次被插入到custom-ul时执行，target是custom-ul节点实例对象，触发在attached生命周期之后
+      },
+      linkChanged: function (target) {
+        // 每次被移动后执行，target是custom-ul节点实例对象，触发在moved生命周期之后
+      },
+      unlinked: function (target) {
+        // 每次被移除时执行，target是custom-ul节点实例对象，触发在detached生命周期之后
+      }
+    }
+  },
   /**
    * 组件的初始数据
    */
   data: {
-
+    selectedClass: ""
   },
-
   /**
    * 组件的方法列表
    */
   methods: {
+    onTap() {
+      let newStatus = !this.properties.selected;
+      this.setData({
+        selected: newStatus,
+        selectedClass: newStatus ? "qb-toggle__selected" : ""
+      });
 
+      this.triggerEvent('change', {
+        selected: newStatus
+      }, {});
+    }
   }
 })
